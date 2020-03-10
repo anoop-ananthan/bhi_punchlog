a4a15c4c4d45b0b9465f54dfaf5700bfbb0343f7
Anoop.P.A <anoop@bluehorizoninfotech.com>
2020-03-10 12:45:01 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/bhi/punchlog/bhi_punchlog/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bhi.punchlog.bhi_punchlog
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
