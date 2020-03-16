04c844418219a59d42d7138f7e6945e875fee2b6
Anoop.P.A <anoop@bluehorizoninfotech.com>
2020-03-16 17:00:22 +0530

    Add polling at one min interval

--- lib/screens/profile/timing.dart
+++ lib/screens/profile/timing.dart
@@ -1,54 +1,61 @@
-import 'package:flutter/material.dart';
 import 'package:bhi_punchlog/data/store.dart';
+import 'package:flutter/material.dart';
 import 'package:bhi_punchlog/globals.dart' as globals;
-import 'package:bhi_punchlog/models/user.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
-class Timing extends StatelessWidget {
-  final AppStore store = globals.store;
+class Timing extends StatefulWidget {
+  Timing({Key key}) : super(key: key);
 
   @override
+  _TimingState createState() => _TimingState();
+}
+
+class _TimingState extends State<Timing> {
+  final AppStore store = globals.store;
+  @override
   Widget build(BuildContext context) {
-    final User user = store.profileUser;
     return Expanded(
-      child: ListView(
-        children: ListTile.divideTiles(
-          color: Colors.grey[400],
-          tiles: [
-            ListTile(
-              leading: Icon(
-                Icons.gavel,
-                color: Colors.brown,
+      child: Observer(
+        builder: (_) => ListView(
+          children: ListTile.divideTiles(
+            color: Colors.grey[400],
+            tiles: [
+              ListTile(
+                leading: Icon(
+                  Icons.gavel,
+                  color: Colors.brown,
+                ),
+                title: Text('First punch'),
+                subtitle: Text(store.profileUser.firstPunch ?? 'Not arrived'),
               ),
-              title: Text('First punch'),
-              subtitle: Text(user.firstPunch ?? 'Not arrived'),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.access_time,
-                color: Colors.purple,
+              ListTile(
+                leading: Icon(
+                  Icons.access_time,
+                  color: Colors.purple,
+                ),
+                title: Text(
+                  'Total time',
+                ),
+                subtitle: Text(store.profileUser.totalTime ?? ''),
               ),
-              title: Text(
-                'Total time',
+              ListTile(
+                leading: Icon(
+                  Icons.laptop_mac,
+                  color: Colors.indigo,
+                ),
+                title: Text(
+                  'Time in office',
+                ),
+                subtitle: Text(store.profileUser.timeInOffice ?? ''),
               ),
-              subtitle: Text(user.totalTime ?? ''),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.laptop_mac,
-                color: Colors.indigo,
-              ),
-              title: Text(
-                'Time in office',
-              ),
-              subtitle: Text(user.timeInOffice ?? ''),
-            ),
-            ListTile(
-              leading: Icon(Icons.fastfood, color: Colors.pink),
-              title: Text('Break time'),
-              subtitle: Text(user.timeForBreak ?? ''),
-            )
-          ],
-        ).toList(),
+              ListTile(
+                leading: Icon(Icons.fastfood, color: Colors.pink),
+                title: Text('Break time'),
+                subtitle: Text(store.profileUser.timeForBreak ?? ''),
+              )
+            ],
+          ).toList(),
+        ),
       ),
     );
   }
